d980bd3e067678432c04bdc1b568f117b2b6cf70
CoryDS79 <coryds79@gmail.com>
2022-04-28 06:59:32 -0400

    Initial Session Data Permanence
    Started process of saving player name between game sessions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] Button startBtn;
     [SerializeField] TMP_InputField nameInputField;
     public string playerName;
+    public TextMeshProUGUI highScoreText;
     public static GameManager instance;
     // Start is called before the first frame update
 
@@ -22,6 +24,8 @@ public class GameManager : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadPlayerName();
     }
 
     void Start()
@@ -40,6 +44,40 @@ public class GameManager : MonoBehaviour
         playerName = nameInputField.text;
         // Debug.Log(playerName);
         SceneManager.LoadScene("main");
+    }
+
+    public void QuitButtonClicked()
+    {
+        LoadPlayerName();
+    }
+
+    [System.Serializable]
+    class SavaData
+    {
+        public string playerNameSaved;
+        public string highScoreSaved;
+    }
+
+    public void SavePlayerName()
+    {
+        SavaData data = new SavaData();
+        data.playerNameSaved = playerName;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadPlayerName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SavaData data = JsonUtility.FromJson<SavaData>(json);
+
+            highScoreText.text = data.playerNameSaved;
+        }
     }
 
 }
